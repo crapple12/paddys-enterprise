7898b001acca20cb6118064f86b1338a76a76dfd
crapple12 <enoestephon@gmail.com>
2026-02-19 00:50:31 -0400

    Updates

--- src/pages/products/products.js
+++
@@ -1,8 +0,0 @@
-
-import "../header.js";
-import "./products.css";
-
-menu.addEventListener("click", () => {
-  const navLinks = document.querySelector(".nav-links");
-  navLinks.classList.toggle("active");
-});
